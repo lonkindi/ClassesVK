6b9a04203df9df158619666d3abbb5b11c5d208f
lonkindi <legions.ru@gmail.com>
2020-02-25 23:46:33 +0500

    Task1

--- classes.py
+++ classes.py
@@ -1,7 +1,13 @@
 import requests
 
 access_token = 'af9c7f37dc361ad97888e979d4be8143d4e6bc1c2466a4722c1f3fefc185e107719ebaa66a8ff92cdf3d8'
-
+"""
+ API_ID, APP_ID, client_id, = 7331927 
+ user_id=24863449 
+ https://oauth.vk.com/blank.html#access_token=af9c7f37dc361ad97888e979d4be8143d4e6bc1c2466a4722c1f3fefc185e107719ebaa66a8ff92cdf3d8&expires_in=0&user_id=24863449&state=123456
+ 
+ https://oauth.vk.com/authorize?client_id=7331927&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=friends,offline&response_type=token&v=5.103&state=123456
+"""
 
 class UserVK:
     def __init__(self, vk_token, user_id=None):
